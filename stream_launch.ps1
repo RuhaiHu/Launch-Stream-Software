23896db8b2ee0f895b15e3054bfd5be9deee38ab
Ruhai Hu <RuhaiHu@users.noreply.github.com>
2018-09-08 23:45:34 -0700

    Updates
        Initial script development
        Clean up of paths
        Update Comments and Descriptions
        Updated environmental paramaters to use Powershell Params instead of Command Line since they were not working.
        Fix Logic that just wasn't working.

--- stream_launch.ps1
+++ stream_launch.ps1
@@ -4,12 +4,22 @@
   <Overview of script>
 
 .DESCRIPTION
-  Kill dropbox / google drive backup / etc
-    These are known to cause issues with game save files
-  Switch power plans
+  Stop Processes that are known to cause issues with game save files.
+    Dropbox
+    Google Backup and Sync    
+  Switch Power Plan
+    From any to HIGH
   Launch software for streaming
-  After OBS and other software is closed relaunch killed application
-
+    StreamLabels
+    StreamLabsChat
+    HexChat
+    Pretzel
+    OBS
+  After Software is closed manually
+    Switch Power Plan
+      From HIGH to Balanced
+    Relaunch closed programs
+  
 .PARAMETER <Parameter_Name>
     None
 
@@ -30,8 +40,10 @@
   Purpose/Change: 
     Initial script development
     Clean up of paths
+    Update Comments and Descriptions
+    Updated environmental paramaters to use Powershell Params instead of Command Line since they were not working.
+    Fix Logic that just wasn't working.
 
-  
 .EXAMPLE
   None
 #>
@@ -67,29 +79,64 @@ Try {
 # Also check to see if they are already running
 
 # Check then Launch Hexchat
-if((Get-Process -Name 'hexchat') -eq $null){
-  $hexchat = Start-Process -FilePath "C:\Program Files\HexChat\hexchat.exe" -WorkingDirectory "C:\Program Files\HexChat"
+if(!(Get-Process -Name 'hexchat')){
+  Start-Process -FilePath "C:\Program Files\HexChat\hexchat.exe" -WorkingDirectory "C:\Program Files\HexChat"
+  if(Get-Process -Name 'hexchat'){
+  Write-Output "HexChat Started!"}
+}
+elseif(Get-Process -Name 'hexchat'){
+  Write-Output "HexChat already running!"}
+else{
+  Write-Error "Hexchat Failed to Start!"
 }
 
 # Check then Launch Stream Labels
-if((Get-Process -Name 'streamlabels') -eq $null){
-  $streamlabels = Start-Process -FilePath "%LOCALAPPDATA%\Programs\streamlabels\StreamLabels.exe" -WorkingDirectory "%LOCALAPPDATA%\Programs\streamlabels"
+if(!(Get-Process -Name 'streamlabels')){
+  # Annoyingly Stream Labs Labels outputs a bunch of connection information so sending it to null
+  Start-Process -FilePath "$env:LOCALAPPDATA\Programs\streamlabels\StreamLabels.exe" -WorkingDirectory "$env:LOCALAPPDATA\Programs\streamlabels" -redirectstandardoutput null
+  if(Get-Process -Name 'streamlabels'){
+  Write-Output "StreamLabels Started!"}
+}
+elseif(Get-Process -Name 'streamlabels'){
+  Write-Output "StreamLabels already running!"}
+else{
+  Write-Error "StreamLabels Failed to Start!"
 }
 
 # Check then Launch Chatbot
-if((Get-Process -Name 'Streamlabs Chatbot') -eq $null){
-  $streamlabschatbot = Start-Process -FilePath "%APPDATA%\Streamlabs\Streamlabs Chatbot\Streamlabs Chatbot.exe" -WorkingDirectory "%APPDATA%\Streamlabs\Streamlabs Chatbot"
+if(!(Get-Process -Name 'Streamlabs*')){
+  Start-Process -FilePath "$env:APPDATA\Streamlabs\Streamlabs Chatbot\Streamlabs Chatbot.exe" -WorkingDirectory "$env:APPDATA\Streamlabs\Streamlabs Chatbot"
+  if(Get-Process -Name 'Streamlabs*'){
+  Write-Output "StreamLabs Chatbot Started!"}
+}
+elseif(Get-Process -Name 'Streamlabs*'){
+  Write-Output "StreamLabs Chatbot already running!"}
+else{
+  Write-Error "StreamLabs Chatbot Failed to Start!"
 }
 
 # Check then Launch Pretzel
-if((Get-Process -Name 'pretzel') -eq $null){
-  $pretzel = Start-Process -FilePath "%LOCALAPPDATA%\Programs\PretzelDesktop\Pretzel.exe" -WorkingDirectory "%LOCALAPPDATA%\Programs\PretzelDesktop"
+if(!(Get-Process -Name 'pretzel')){
+  Start-Process -FilePath "$env:LOCALAPPDATA\Programs\PretzelDesktop\Pretzel.exe" -WorkingDirectory "$env:LOCALAPPDATA\Programs\PretzelDesktop"
+  if(Get-Process -Name 'pretzel'){
+  Write-Output "Pretzel Started"}
+}
+elseif(Get-Process -Name 'pretzel'){
+  Write-Output "Pretzel already running!"}
+else{
+  Write-Error "Pretzel Failed to Start!"
 }
 
-# Launch streaming program and wait for it to exit
-# ofcourse make sure it is not running also
-if((Get-Process -Name 'obs64') -eq $null){
-  $obs = Start-Process -FilePath "C:\Program Files (x86)\obs-studio\bin\64bit\obs64.exe" -WorkingDirectory "C:\Program Files (x86)\obs-studio\bin\64bit" -Verb runAs
+# Check for and then launch OBS
+if(!(Get-Process -Name 'obs64')){
+  Start-Process -FilePath "C:\Program Files (x86)\obs-studio\bin\64bit\obs64.exe" -WorkingDirectory "C:\Program Files (x86)\obs-studio\bin\64bit" -Verb runAs
+  if(Get-Process -Name 'obs64'){
+  Write-Output "OBS Started"}
+}
+elseif(Get-Process -Name 'obs64'){
+  Write-Output "OBS already running"}
+else{
+  Write-Error "OBS Failed to Start!"
 }
 
 # While needed stoftware for streaming running
@@ -103,21 +150,20 @@ do{
   `n Sleeping for 2 minutes.
   `n Before relaunching closed programs.
   `n Sleep will repeat until programs close."
-  Start-Sleep -seconds 120
+  Start-Sleep -seconds 10
 
   # Determine if processes are running and add them to count
   # So we can determine if we want to continue to wait
-  if((Get-Process -Name 'obs64').length -gt 0){
+  if(Get-Process -Name 'obs64'){
       $running += (Get-Process -Name 'obs64').length
   }
-  if((Get-Process -Name 'HexChat').length -gt 0){
+  if(Get-Process -Name 'HexChat'){
       $running += (Get-Process -Name 'HexChat').length
   }
-  if((Get-Process -Name 'streamlab*').length -gt 0){
+  if(Get-Process -Name 'streamlab*'){
       $running += (Get-Process -Name 'streamlab*').length
   }
   Write-OutPut " Number of running programs: $running"
-
 }while($running -gt 0)
 
 # Created the daily as a clone of my balance performance settings
@@ -132,7 +178,9 @@ Try {
 }
 
 # Relaunch killed process from above if not already running
-if((Get-Process -Name 'dropbox') -eq $null){
-  $hexchat = Start-Process -FilePath "C:\Program Files (x86)\Dropbox\Client\Dropbox.exe" -WorkingDirectory "C:\Program Files (x86)\Dropbox\Client" }
-if((Get-Process -Name 'googledrivesync') -eq $null){
-  $hexchat = Start-Process -FilePath "C:\Program Files\Google\Drive\googledrivesync.exe" -WorkingDirectory "C:\Program Files\Google\Drive\" }
+if(!(Get-Process -Name 'dropbox')){
+  Start-Process -FilePath "C:\Program Files (x86)\Dropbox\Client\Dropbox.exe" -WorkingDirectory "C:\Program Files (x86)\Dropbox\Client"
+}
+if(!(Get-Process -Name 'googledrivesync')){
+  Start-Process -FilePath "C:\Program Files\Google\Drive\googledrivesync.exe" -WorkingDirectory "C:\Program Files\Google\Drive\"
+}
